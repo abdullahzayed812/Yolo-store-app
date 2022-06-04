17b2c3e13d53dec70f1a29a2c38372c32ca6223a
Abdullah Zayed <github/abdullahzayed812>
2022-06-04 10:05:05 +0200

    update the home page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -8,7 +8,7 @@ import { Cart } from "../pages/Cart";
 export function RoutesComponent() {
   return (
     <Routes>
-      <Route exact path="/" element={<Home />} />
+      <Route exact path="/abdullahzayed812.github.io/Yolo-store-app/" element={<Home />} />
       <Route path="/catalog" element={<Catalog />} />
       <Route path="/catalog/:slug" element={<Product />} />
       <Route path="/cart" element={<Cart />} />
